97d5bae683e683192e472fea69e7b57c815037e2
MystProgrammer <animeshdeb89@gmail.com>
2025-12-05 19:52:35 -0500

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Vibe Check Backend is Alive');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
