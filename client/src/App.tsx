31dbffe51992bb71bcb2f23c8975d7e98c61104b
MystProgrammer <animeshdeb89@gmail.com>
2025-12-06 10:25:05 -0500

    Fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import Landing from './components/landing'
-import Bio from './components/signup'
+import Bio from './components/bio'
 import { SignedIn } from '@clerk/clerk-react'
 import Signin from './components/signin'
 import Signup from './components/signup'
-function App() {
 
+// 1. Define the wrapper component to use the hook
+function BioWrapper() {
+  const navigate = useNavigate();
+  
+  // Define the success handler: navigate to the home dashboard
+  const handleSuccess = () => {
+    navigate('/home'); 
+  };
+  
+  // Pass the handler down to the Bio component
   return (
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<Landing/>}/>
-  <Route path="/signin/*" element={<Signin/>}/>
-  <Route path="/signup/*" element={<Signup/>}/>
-  <Route path="/bio" element={<><SignedIn><Bio/></SignedIn></>}/>
-</Routes>
+    <SignedIn>
+      {/* 2. Pass the required prop here */}
+      <Bio onSuccess={handleSuccess} /> 
+    </SignedIn>
+  );
+}
 
-</BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/signin/*" element={<Signin/>}/>
+        <Route path="/signup/*" element={<Signup/>}/>
+        
+        {/* 3. Render the wrapper component in the route */}
+        <Route path="/bio" element={<BioWrapper />} /> 
+
+        {/* Note: You need a route for /home, which Bio redirects to after success */}
+        <Route path="/home" element={<SignedIn><div>Welcome Home!</div></SignedIn>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
